7168403ee4ae8805d267dfae14a545971d96eb0f
lyc-chengzi <liuyc14@lenovo.com>
2016-10-09 15:42:41 +0800

    demo3演示嵌套结构

--- src/pages/demo3/index.js
+++ src/pages/demo3/index.js
@@ -1,9 +1,56 @@
 /**
  * Created by liuyc14 on 2016/9/28.
  */
-import * as redux from 'redux';
+import todo from '../../redux/Todo/TodoReducer';
+import action from '../../redux/Todo/TodoAction';
 import city from '../../redux/CityReducer';
 import counter from '../../redux/CounterReducer';
+import {TodoListReducer} from '../../redux/Todo/TodoListReducer';
+import * as redux from 'redux';
+
+/*
+ console.log(todo);
+ //嵌套结构
+ let app = redux.combineReducers({
+ city: city,
+ counter: counter,
+ todo: todo
+ });
+
+ let store = redux.createStore(app);
+
+ store.subscribe(function(){
+ console.log(store.getState());
+ });
+
+ store.dispatch(action.create('第1个任务'));
+ store.dispatch(action.create('第2个任务'));
+ store.dispatch(action.create('第3个任务'));
+ */
+const _todo = function(todo = {}, action){
+    return {
+        counter: counter(todo.counter, action),
+        todos: TodoListReducer(todo.todos, action)
+    }
+}
+
+const app2 = function(state={}, action){
+    return {
+        city: city(state.city, action),
+        todo: _todo(state.todo, action)
+    };
+}
+
+let store2 = redux.createStore(app2);
+store2.subscribe(function(){
+    console.log(store2.getState());
+});
+
+store2.dispatch(action.create('第1个任务'));
+store2.dispatch(action.create('第2个任务'));
+store2.dispatch(action.create('第3个任务'));
+
+store2.dispatch({type:'counter_add'})
 
 let app = redux.combineReducers({city, counter});
 console.log('------------log Object:app------------');
